bbc9636cb1a6b22a4483a29bf12ade371827528e
bhuwan <bhuwanaryal5555@gmail.com>
2021-04-01 13:25:50 +0545

    first commit

---
+++ home/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.urls import reverse
+
+STATUS = (('active','active'),('passive','passive'))
+LABEL = (('new','new'),('hot','hot'),('sale','sale'),('','default'))
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.CharField(max_length=300)
+    slug = models.CharField(max_length=300,unique=True)
+    status = models.CharField(choices=STATUS,max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Slider(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField(blank=True)
+    status = models.CharField(choices=STATUS, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    rank = models.IntegerField()
+    image = models.ImageField(upload_to= 'media')
+    description = models.TextField(blank=True)
+    status = models.CharField(choices=STATUS, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='media')
+    status = models.CharField(choices=STATUS, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField(default=0)
+    label = models.CharField(choices= LABEL,max_length=200)
+    image = models.ImageField(upload_to='media')
+    status = models.CharField(choices=STATUS, max_length=200)
+    slug = models.CharField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE,null = True)
+
+
+    def __str__(self):
+        return self.title
+    def get_item_url(self):
+        return reverse("home:products",kwargs = {'slug':self.slug})
+
---
+++ home/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+from .models import *
+
+# Create your views here.
+class BaseView(View):
+    views = {}
+
+class HomeView(BaseView):
+    def get(self, request):
+        self.views['categories'] = Category.objects.filter(status='active')
+        self.views['sliders'] = Slider.objects.filter(status = 'active')
+        self.views['brands'] = Brand.objects.filter(status = 'active')
+        self.views['ads1'] = Ad.objects.all()
+        self.views['hots'] = Item.objects.filter(label = 'hot')
+        self.views['news'] = Item.objects.filter(label = 'new')
+        self.views['sales'] = Item.objects.filter(label = 'sale')
+        self.views['defaults'] = Item.objects.filter(label='')
+        return render(request,'index.html',self.views)
+
+class ItemDetailView(BaseView):
+    def get(self,request,slug):
+        self.views['item_detail'] = Item.objects.filter(slug = slug)
+        self.views['brand'] = Brand.objects.filter(status = 'active')
+
+        self.views['count'] = []
+        for i in self.views['brand']:
+            count_food = Item.objects.filter(brand = i.id).count()
+            print(i.name,count_food)
+            d = {'name':i.name,'count':count_food}
+            self.views['count'].append(d)
+
+
+            self.views['count_cat'] = Category.objects.filter(status='active')
+            self.views['cat_count'] = []
+            for i in self.views['count_cat']:
+                count_food = Item.objects.filter(category=i.id).count()
+                print(i.name, count_food)
+                dd = {'name': i.name,'image':i.image,'cat_count': count_food}
+                self.views['cat_count'].append(d)
+
+
+
+        cat = Item.objects.get(slug = slug).category_id
+        self.views['catitems'] = Item.objects.filter(category = cat)
+
+        return render(request,'product-detail.html',self.views)
+
+
+
